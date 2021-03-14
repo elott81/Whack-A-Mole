0607a2ff429867ef037041e4046ea066bbc39039
Ed Lott <elott81@gmail.com>
2021-03-14 19:28:08 -0400

    loop random animation

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 const moles = document.querySelectorAll('.mole');
 
+// function animateMoles() {
+//     let rand = Math.floor(Math.random() * 6)
+//     moles[rand].classList.toggle('mole-animate');
+// }
+
 function animateMoles() {
-    let rand = Math.floor(Math.random() * 6)
-    moles[rand].classList.toggle('mole-animate');
+    setTimeout(function () {
+        let rand = Math.floor(Math.random() * 6);
+        moles[rand].classList.toggle('mole-animate');
+        animateMoles();
+    }, 800);
 }
 
 animateMoles();
